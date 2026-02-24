8a5f23b617d02f8fcd930c06262904c844ed2df2
Harun <theayyyron@gmail.com>
2026-02-24 14:34:29 +0800

    Add POST route for adding new question

--- question-service/controllers/questionController.js
+++ question-service/controllers/questionController.js
@@ -8,6 +8,32 @@ const getAllQuestions = async (req, res) => {
     res.status(200).json(questions)
 }
 
+const addQuestion = async (req, res) => {
+    const { title, question, answer, difficulty, category, tags, examples } = req.body
+
+    if (!title || !question || !answer || !difficulty || !category) {
+        res.status(400).json({ message: 'Please provide all required fields' })
+        return
+    }   
+
+    try {
+        const newQuestion = await Question.create({
+            title,
+            question,
+            answer,
+            difficulty,
+            category,
+            tags,
+            examples,
+        })
+        res.status(201).json(newQuestion)
+    } catch (err) {
+        res.status(400).json({ message: err.message })
+    }   
+}   
+
+
 module.exports = {
     getAllQuestions,
+    addQuestion,
 }
--- question-service/routes/questionRoutes.js
+++ question-service/routes/questionRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllQuestions } = require('../controllers/questionController');
+const { getAllQuestions, addQuestion } = require('../controllers/questionController');
 
 router.route('/').get(getAllQuestions)
+router.route('/').post(addQuestion)
 
 module.exports = router;
